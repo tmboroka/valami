248172fff0b6483a4af867da36529e852c5ae3e4
PeterfiLaci <113706063+placi0325@users.noreply.github.com>
2023-02-07 11:17:41 +0100

    add feat filter by type

--- filterByType.js
+++ filterByType.js
@@ -2,10 +2,11 @@ const getBeers = require('./data')
 
 function main(type) {
     console.log('type:', type)
-
+    const beers = getBeers();
+    return beers.filter((beer) => beer.type.includes(type))
 }
+console.log(main('Ale'));
 
-main(process.argv[2])
 
 // Usage: node filterByType.js <type>
 // Example: node filterByType.js ALE
